8a5388f8fffd9e6199c2cfe502d49bb118343c2d
Jürgen Gutsch <juergen.gutsch@yoo.digital>
2025-07-24 08:26:54 +0200

    initial commit

---
+++ src/Tests/UITests/Helper/TestHelper.cs
@@ -0,0 +1,28 @@
+﻿using Gallio.Common.Markup;
+using Gallio.Framework;
+using WatiN.Core;
+
+namespace UITests.Helper
+{
+    public class TestHelper
+    {
+
+        public static void Snapshot(IE ie, string caption)
+        {
+            Snapshot(ie, caption, TestLog.Default);
+        }
+
+        public static void Snapshot(IE ie, string caption, MarkupStreamWriter logStreamWriter)
+        {
+            using (logStreamWriter.BeginSection(caption))
+            {
+                logStreamWriter.Write("Url: ");
+                using (logStreamWriter.BeginMarker(Marker.Link(ie.Url)))
+                    logStreamWriter.Write(ie.Url);
+                logStreamWriter.WriteLine();
+
+                logStreamWriter.EmbedImage(caption + ".png", new CaptureWebPage(ie).CaptureWebPageImage(false, false, 100));
+            }
+        }
+    }
+}
---
+++ src/Tests/UITests/Helper/WatiNStreamLogger.cs
@@ -0,0 +1,13 @@
+﻿using Gallio.Framework;
+using WatiN.Core.Interfaces;
+
+namespace UITests.Helper
+{
+    public class WatiNStreamLogger : ILogWriter
+    {
+        public void LogAction(string message)
+        {
+            TestLog.WriteLine(message);
+        }
+    }
+}
